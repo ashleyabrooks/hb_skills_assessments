bbe49f261be93e14b2d772d3abb4bb0ddb6e10bd
Ashley Brooks <ashley.brooks.a@gmail.com>
2017-01-21 15:51:16 -0800

    Added Jinja2 templating and created application success page.

--- flask/server.py
+++ flask/server.py
@@ -3,13 +3,38 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 
 app = Flask(__name__)
-
-
-# Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
 
-# YOUR ROUTES GO HERE
+@app.route('/')
+def index():
+    """Return HTML for homepage."""
+
+    return render_template("index.html")
+
+
+@app.route('/application-form')
+def show_application_form():
+    """Return HTML for application form using POST method."""
+
+    jobtitles = ['Software Engineer', 'QA Engineer', 'Product MAanager']
+
+    return render_template("application-form.html", jobtitles=jobtitles)
+
+
+@app.route('/application-success', methods=["POST"])
+def show_app_success():
+    """Return customized application-success page with user's information from application form."""
+    
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    jobtitle = request.form.get("jobtitle")
+    salaryreq = request.form.get("salaryreq")
+
+    return render_template("application-response.html", firstname=firstname,
+                                                        lastname=lastname,
+                                                        jobtitle=jobtitle,
+                                                        salaryreq=salaryreq)
 
 
 if __name__ == "__main__":
